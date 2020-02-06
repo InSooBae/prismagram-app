9c740229f830784a9ea3becf2c6f6cb1e5fcab4a
InSooBae <qospwmf@gmail.com>
2020-02-07 00:51:53 +0900

    add withNavigation for Message Navigator in TabNavigator at Home's headerRight

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -2,13 +2,15 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import TabNavigation from './TabNavigation';
 import PhotoNavigation from './PhotoNavigation';
+import MessageNavigation from './MessageNavigation';
 
 //TabNavigation이 MainNavigation의 자식이 됨
 //MainNavigation도 stackNavigtor라서 다른 헤더를 가지고있음 이게 밖에있는 PhotoNavigation의 header
 const MainNavigation = createStackNavigator(
   {
     TabNavigation,
-    PhotoNavigation
+    PhotoNavigation,
+    MessageNavigation
   },
   {
     headerMode: 'none',
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,19 +1,42 @@
-import { View } from 'react-native';
+import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import Home from '../screens/Home';
 import Notifications from '../screens/Notifications';
 import Profile from '../screens/Profile';
 import Search from '../screens/Search';
+import MessagesLink from '../components/MessagesLink';
+import { View } from 'react-native';
+
+//TabNavigator에 각 탭마다 StackNavigator효과를 주는일 customconfig에는설정들
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    InitialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig }
+    }
+  });
 
 export default createBottomTabNavigator({
-  Home,
-  Notifications,
+  Home: {
+    screen: stackFactory(Home, {
+      title: 'Home',
+      headerRight: () => <MessagesLink />
+    })
+  },
+  Search: {
+    screen: stackFactory(Search, { title: 'Search ' })
+  },
   Add: {
     screen: View,
     navigationOptions: {
       tabBarOnPress: ({ navigation }) => navigation.navigate('PhotoNavigation')
     }
   },
-  Profile,
-  Search
+  Notifications: {
+    screen: stackFactory(Notifications, { title: 'Notifications' })
+  },
+  Profile: {
+    screen: stackFactory(Profile, { title: 'Profile' })
+  }
 });
