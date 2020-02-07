4b47ac7830aa163d6383c0a91eb4fa5cff662b19
InSooBae <qospwmf@gmail.com>
2020-02-08 03:17:52 +0900

    recover for Login's Error (props:onEndEditing -> onSubmitEditing) and add mutation(gql)

--- components/AuthInput.js
+++ components/AuthInput.js
@@ -15,19 +15,29 @@ const TextInput = styled.TextInput`
 `;
 
 //onChangeText는 callback은 input이 변경될때 호출된다.
+//onEndEditing는 textInput이 끝나면 콜백함수로 실행된다
+//autoCorrect 는 자동수정기능이다.
+//onEndEditing 문제점 발견! 누군가 send를 눌렀는지 아닌지 감지하려는 목적으로 쓰면안됨
+// send는 onEndEditing 의 계기가 되지만 input의 바깥에서도 실행되도록 세팅되있음 -> onSubmitEditing 쓸거
 const AuthInput = ({
   placeholder,
   value,
   keyboardType = 'default',
   autoCapitalize = 'none',
-  onChange
+  onChange,
+  returnKeyType = 'done',
+  onSubmitEditing = () => null,
+  autoCorrect = true
 }) => (
   <Container>
     <TextInput
       onChangeText={onChange}
       keyboardType={keyboardType}
       placeholder={placeholder}
+      returnKeyType={returnKeyType}
       value={value}
+      onSubmitEditing={onSubmitEditing}
+      autoCorrect={autoCorrect}
       autoCapitalize={autoCapitalize}
     />
   </Container>
@@ -45,7 +55,10 @@ AuthInput.propTypes = {
     'phone-add'
   ]),
   autoCapitalize: PropTypes.oneOf(['none', 'sentences', 'words', 'characters']),
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  returnKeyType: PropTypes.oneOf(['done', 'go', 'next', 'search', 'send']),
+  onSubmitEditing: PropTypes.func,
+  autoCorrect: PropTypes.bool
 };
 
 export default AuthInput;
--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import { useMutation } from '@apollo/react-hooks';
 import AuthButton from '../../components/AuthButton';
+import { LOG_IN } from './AuthQueries';
 import AuthInput from '../../components/AuthInput';
 import useInput from '../../hooks/useInput';
 
@@ -12,16 +15,58 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-export default () => {
+export default ({ navigation }) => {
   const emailInput = useInput('');
+  const [loading, setLoading] = useState(false);
+  const [requestSecretMutation] = useMutation(LOG_IN, {
+    variables: {
+      email: emailInput.value
+    }
+  });
+  // 이메일이 유효한지 검증
+  const handleLogin = async () => {
+    const { value } = emailInput;
+    //이메일 99.99% 유효성 체크
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (value === '') {
+      return Alert.alert("Email can't be empty");
+    } else if (!value.includes('@') || !value.includes('.')) {
+      return Alert.alert('Please Write an Email');
+    } else if (!emailRegex.test(value)) {
+      return Alert.alert('That email is Invalid');
+    }
+    try {
+      setLoading(true);
+      const {
+        data: { requestSecret }
+      } = await requestSecretMutation();
+      if (requestSecret) {
+        Alert.alert('Check Your Email');
+        navigation.navigate('Confirm');
+        return;
+      } else {
+        Alert.alert('Account not Found');
+        navigation.navigate('SignUp');
+      }
+    } catch (e) {
+      Alert.alert("Can't Log In Now!");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <View>
-      <AuthInput
-        {...emailInput}
-        placeholder="Email"
-        keyboardType="email-address"
-      />
-      <AuthButton onPress={() => null} text={'Log In'} />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View>
+        <AuthInput
+          {...emailInput}
+          placeholder="Email"
+          keyboardType="email-address"
+          returnKeyType="send"
+          onSubmitEditing={handleLogin}
+          autoCorrect={false}
+        />
+        <AuthButton loading={loading} onPress={handleLogin} text={'Log In'} />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
